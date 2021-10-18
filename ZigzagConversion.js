5e85d2070db3c918be5f07780af918a8bacaee4f
Alex <alextheprogrammer21@gmail.com>
2021-10-18 08:22:07 -0700

    complete zigzag conversion

--- ZigzagConversion.js
+++ ZigzagConversion.js
@@ -1,37 +1,53 @@
 var convert = function(s, numRows) {
- let y = 0;
- let isZagging = true; 
- const rows = {};
- 
- for (let i = 0; i < s.length; i++) {
-     const s[i] = char;
-     
-     if (y >= numRows) {
-         isZagging = false;
-         y--;
-     }
-     if (y === 0) {
-         isZagging = true;
-     }
-     
-     if (isZagging) {
-         if (rows[y]) {
-             rows[y] += char;
-         } else {
-         rows[y] = char;
-         }
-         y++
-     }
-     if (!isZagging) {
-         if (rows[y]) {
-             rows[y] += char;
-         } else {
-         rows[y] = char;
-         }
-         y--;
-     }
-     
- }
- //Turn the final object into a single return string
- //Check to see if all your y-- and y++ things are correctly numbered
+    let y = 1;
+    let isZagging = true; 
+    const rows = {};
+    
+    if (s.length < 2) {
+        return s;
+    }
+
+    if (numRows < 2) {
+        return s;
+    }
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        
+        if (isZagging) {
+            if (rows[y]) {
+                rows[y] += char;
+            } else {
+            rows[y] = char;
+            }
+            y++
+        }
+        if (!isZagging) {
+            if (rows[y]) {
+                rows[y] += char;
+            } else {
+            rows[y] = char;
+            }
+            y--;
+        }
+        
+                if (y > numRows) {
+            isZagging = false;
+            y = y-2;
+        }
+
+
+        if (!isZagging && y === 1) {
+            isZagging = true;
+            
+        }
+    }
+    const keys = Object.keys(rows);
+    let finalString = "";
+    console.log(keys)
+    keys.forEach((key) => {
+    finalString += rows[key]
+    console.log(rows[key])
+})
+    return finalString;
 };
+
