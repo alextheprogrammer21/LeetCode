21d5ee4e08d76d12eb8dd50c1a22a0b9ce0db8d8
Alex <alextheprogrammer21@gmail.com>
2021-11-23 08:11:27 -0800

    generateparenthesis

---
+++ generateParantheses.js
@@ -0,0 +1,10 @@
+var generateParenthesis = function(n) {
+    
+    const solution = [];
+
+    function helper(left, right, str) {
+        if (left > right) return;
+        if (!left && !right) solution.push(str)
+        if ()
+    }
+};
