387c7e55d45b3ba0bf1f043cdfd1bb493eb9aee4
Alex <alextheprogrammer21@gmail.com>
2021-11-23 08:13:28 -0800

    generateparantehesis

--- generateParantheses.js
+++ generateParantheses.js
@@ -5,6 +5,11 @@ var generateParenthesis = function(n) {
     function helper(left, right, str) {
         if (left > right) return;
         if (!left && !right) solution.push(str)
-        if ()
+        if (left > 0) helper(left-1, right, '(')
+        if (right > 0) helper(left, right-1, ')' )
     }
+    helper(n,n,'')
+    return solution;
 };
+
+console.log(generateParenthesis(3))
