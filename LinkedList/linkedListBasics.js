e485870fca8f9358b24dc4adde5d67df7a92c88c
Alex <alextheprogrammer21@gmail.com>
2021-12-02 21:51:25 -0800

    shift

--- LinkedList/linkedListBasics.js
+++ LinkedList/linkedListBasics.js
@@ -23,10 +23,44 @@ class LinkedList {
         this.length++;
         return this;
     }
+    pop() {
+
+        if (!this.head) {
+            return undefined;
+        }
+
+        let current = this.head; 
+        while (current.next) {
+
+            if (!current.next.next) {
+                let temp = current.next;
+                current.next = null;
+                this.tail = current;
+                this.length--;
+                return temp;
+            }
+            current = current.next;
+
+        }
+    }
+    shift() {
+        if (!this.head) return null;
+
+        let temp = this.head;
+        this.head = temp.next;
+        this.length--;
+        if (this.length === 0) {
+            this.tail = null;
+        }
+        return temp;
+    }
 }
 
 const list = new LinkedList();
 list.push("HI");
 list.push("HE");
+list.push("LULLERS");
 
+console.log(list);
+console.log(list.shift());
 console.log(list);
